e0baa1bf7e1a3dc87f01493c88ddd79ace3fe9b7
Nimajavanbakht02 <nimabrah19@gmail.com>
2024-08-25 16:49:41 -0700

    completes expense tracker

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
-import React, { createContext, userReducer } from 'react';
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 // initial state
 const initialState = {
@@ -8,4 +9,29 @@ const initialState = {
         { id: 3, text: 'Book', amount: -10 },
         { id: 4, text: 'Camera', amount: 150 },
     ]
+}
+
+// Create Context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({ 
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction) {
+        dispatch({ 
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    return (<GlobalContext.Provider value={{ transactions:state.transactions, deleteTransaction, addTransaction }}> {children} </GlobalContext.Provider>)
 }
